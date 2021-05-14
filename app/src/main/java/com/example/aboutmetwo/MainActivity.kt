a0e65141fa88973741e2a87f3bf3c8cdf428a7c5
LiZhifeng <markcat@live.com>
2021-05-14 16:26:27 +0800

    error

--- app/src/main/java/com/example/aboutmetwo/MainActivity.kt
+++ app/src/main/java/com/example/aboutmetwo/MainActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.aboutmetwo
 
 import android.content.Context
@@ -9,6 +25,17 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.aboutmetwo.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
+
+
+import android.content.Context
+import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.android.aboutme.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.myName = myName
 
